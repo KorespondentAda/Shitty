2b6d72784a7add2dd4c12cc53eee8e2334901f3b
Fikafusik <fikafusik@gmail.com>
2018-02-24 14:43:06 +0300

    BITMAPCreate function

--- src/main.c
+++ src/main.c
@@ -5,78 +5,37 @@
 
 #define IMAGEPATH "img/img05.bmp"
 
+void BITMAPCreate(BITMAP * bitmap, const char * path);
+
 int main() {
 
     printf("BITMAPINFO size = %ld: \t%s\n", sizeof(BITMAPINFO), sizeof(BITMAPINFO) == 124 ? "ok" : "error");
 
-    FILE * image_file = fopen(IMAGEPATH, "r");
-    if (image_file == NULL) {
-        printf("Can not open image file <%s>...\n", IMAGEPATH);
-        return 0;
-    }
-
-    BITMAPFILEHEADER BMPHEADER;
-    fread(&BMPHEADER.bfType,        sizeof(BMPHEADER.bfType),       1, image_file);
-    if (BMPHEADER.bfType != 0x4D42 && BMPHEADER.bfType != 0x424D) {
-        printf("File <%s> is not bitmap image...", IMAGEPATH);
-        return 0;
-    }
-    fread(&BMPHEADER.bfSize,        sizeof(BMPHEADER.bfSize),       1, image_file);
-    fread(&BMPHEADER.bfReserved1,   sizeof(BMPHEADER.bfReserved1),  1, image_file);
-    fread(&BMPHEADER.bfReserved2,   sizeof(BMPHEADER.bfReserved2),  1, image_file);
-    fread(&BMPHEADER.bfOffBits,     sizeof(BMPHEADER.bfOffBits),    1, image_file);
+    BITMAP bitmap;
+    BITMAPCreate(&bitmap, IMAGEPATH);
+    BITMAPFILEHEADER file_header = bitmap.BMPHeader;
+    BITMAPINFO       info_header = bitmap.BMPInfo;
     
     printf("\t--BITMAPFILEHEADER--\nbfType\t\t%X\nbfSize\t\t%d\nbfReserved1\t%d\nbfReserved2\t%d\nbfOffBits\t%d\n\n", 
-        BMPHEADER.bfType, BMPHEADER.bfSize, BMPHEADER.bfReserved1, BMPHEADER.bfReserved2, BMPHEADER.bfOffBits);
-
-    BITMAPINFO BMPINFO;
-//  if (BMPINFO.biSize >= 12)
-    fread(&BMPINFO.biSize,          sizeof(BMPINFO.biSize),         1, image_file);
-    fread(&BMPINFO.biWidth,         sizeof(BMPINFO.biWidth),        1, image_file);
-    fread(&BMPINFO.biHeight,        sizeof(BMPINFO.biHeight),       1, image_file);
-    fread(&BMPINFO.biPlanes,        sizeof(BMPINFO.biPlanes),       1, image_file);
-    fread(&BMPINFO.biBitCount,      sizeof(BMPINFO.biBitCount),     1, image_file);
-
+        file_header.bfType, file_header.bfSize, file_header.bfReserved1, file_header.bfReserved2, file_header.bfOffBits);
+    /*
     printf("\t--BITMAPINFO--\nbiSize\t\t%d\nbiWidth\t\t%d\nbiHeight\t%d\nbiPlanes\t%d\nbiBitCount\t%d\n\n",
         BMPINFO.biSize, BMPINFO.biWidth, BMPINFO.biHeight, BMPINFO.biPlanes, BMPINFO.biBitCount);
 
     if (BMPINFO.biSize >= 40) {
-        fread(&BMPINFO.biCompression,   sizeof(BMPINFO.biCompression),      1, image_file);
-        fread(&BMPINFO.biSizeImage,     sizeof(BMPINFO.biSizeImage),        1, image_file);
-        fread(&BMPINFO.biXPelsPerMeter, sizeof(BMPINFO.biXPelsPerMeter),    1, image_file);
-        fread(&BMPINFO.biYPelsPerMeter, sizeof(BMPINFO.biYPelsPerMeter),    1, image_file);
-        fread(&BMPINFO.biClrUsed,       sizeof(BMPINFO.biClrUsed),          1, image_file);
-        fread(&BMPINFO.biClrImportant,  sizeof(BMPINFO.biClrImportant),     1, image_file);
-
         printf("biCompression\t%d\nbiSizeImage\t%d\nbiXPelsPerMeter\t%d\nbiYPelsPerMeter\t%d\nbiClrUsed\t%d\nbiClrImportant\t%d\n\n",
             BMPINFO.biCompression, BMPINFO.biSizeImage, BMPINFO.biXPelsPerMeter, BMPINFO.biYPelsPerMeter, BMPINFO.biClrUsed, BMPINFO.biClrImportant);
     }
 
     if (BMPINFO.biSize >= 52) {
-        fread(&BMPINFO.biRedMask,       sizeof(BMPINFO.biRedMask),      1, image_file);
-        fread(&BMPINFO.biGreenMask,     sizeof(BMPINFO.biGreenMask),    1, image_file);
-        fread(&BMPINFO.biBlueMask,      sizeof(BMPINFO.biBlueMask),     1, image_file);
-        
         printf("biRedMask\t%d\nbiGreenMask\t%d\nbiBlueMask\t%d\n", BMPINFO.biRedMask, BMPINFO.biGreenMask, BMPINFO.biBlueMask);
     }
 
     if (BMPINFO.biSize >= 56) {
-        fread(&BMPINFO.biAlphaMask,     sizeof(BMPINFO.biAlphaMask),    1, image_file);
-
         printf("biAlphaMask\t%d\n", BMPINFO.biAlphaMask);
     }
 
     if (BMPINFO.biSize >= 108) {
-        fread(&BMPINFO.biRedMask,       sizeof(BMPINFO.biRedMask),      1, image_file);
-        fread(&BMPINFO.biGreenMask,     sizeof(BMPINFO.biGreenMask),    1, image_file);
-        fread(&BMPINFO.biBlueMask,      sizeof(BMPINFO.biBlueMask),     1, image_file);
-        fread(&BMPINFO.biAlphaMask,     sizeof(BMPINFO.biAlphaMask),    1, image_file);
-        fread(&BMPINFO.biCSType,        sizeof(BMPINFO.biCSType),       1, image_file);
-        fread(&BMPINFO.biEndpoints,     sizeof(BMPINFO.biEndpoints),    1, image_file);
-        fread(&BMPINFO.biGammaRed,      sizeof(BMPINFO.biGammaRed),     1, image_file);
-        fread(&BMPINFO.biGammaGreen,    sizeof(BMPINFO.biGammaGreen),   1, image_file);
-        fread(&BMPINFO.biGammaBlue,     sizeof(BMPINFO.biGammaBlue),    1, image_file);
-
         printf("biCSType\t%d\nbiEndpoints:\n\tRed:\n\t\tX\t%d\n\t\tY\t%d\n\t\tZ\t%d\n\tGreen:\n\t\tX\t%d\n\t\tY\t%d\n\t\tZ\t%d\n\tBlue:\n\t\tX\t%d\n\t\tY\t%d\n\t\tZ\t%d\nbiGammaRed\t%d\nbiGammaGreen\t%d\nbiGammaBlue\t%d\n\n",
             BMPINFO.biCSType,   (int)BMPINFO.biEndpoints.ciexyzRed.ciexyzX, 
                                 (int)BMPINFO.biEndpoints.ciexyzRed.ciexyzY, 
@@ -91,15 +50,80 @@ int main() {
     }
 
     if (BMPINFO.biSize >= 124) {
-        fread(&BMPINFO.biIntent,          sizeof(BMPINFO.biIntent),         1, image_file);
-        fread(&BMPINFO.biProfileData,          sizeof(BMPINFO.biProfileData),         1, image_file);
-        fread(&BMPINFO.biProfileSize,          sizeof(BMPINFO.biProfileSize),         1, image_file);
-        fread(&BMPINFO.biReserved,          sizeof(BMPINFO.biReserved),         1, image_file);
-
         printf("biIntent\t%d\nbiProfileData\t%d\nbiProfileSize\t%d\nbiReserved\t%d\n\n",
             BMPINFO.biIntent, BMPINFO.biProfileData, BMPINFO.biProfileSize, BMPINFO.biReserved);
     }
-
-    printf("File %sclosed.\n", fclose(image_file) == 0 ? "" : "not :) ");
+    */
     return 0;
+}
+
+void BITMAPCreate(BITMAP * bitmap, const char * path) {
+    FILE * bitmap_file = fopen(path, "r");
+    if (bitmap_file == NULL) {
+        printf("<%s>: Can't open image file...\n", path);
+        return;
+    }
+    
+    fread(&bitmap->BMPHeader.bfType,        sizeof(bitmap->BMPHeader.bfType),       1, bitmap_file);
+    if (bitmap->BMPHeader.bfType != 0x4D42 && bitmap->BMPHeader.bfType != 0x424D) {
+        printf("<%s>: File is not bitmap image...", path);
+        return;
+    }
+    fread(&bitmap->BMPHeader.bfSize,        sizeof(bitmap->BMPHeader.bfSize),       1, bitmap_file);
+    fread(&bitmap->BMPHeader.bfReserved1,   sizeof(bitmap->BMPHeader.bfReserved1),  1, bitmap_file);
+    fread(&bitmap->BMPHeader.bfReserved2,   sizeof(bitmap->BMPHeader.bfReserved2),  1, bitmap_file);
+    fread(&bitmap->BMPHeader.bfOffBits,     sizeof(bitmap->BMPHeader.bfOffBits),    1, bitmap_file);
+    
+    // Reading V-CORE
+//  if (BMPINFO.biSize >= 12)
+    fread(&bitmap->BMPInfo.biSize,          sizeof(bitmap->BMPInfo.biSize),         1, bitmap_file);
+    fread(&bitmap->BMPInfo.biWidth,         sizeof(bitmap->BMPInfo.biWidth),        1, bitmap_file);
+    fread(&bitmap->BMPInfo.biHeight,        sizeof(bitmap->BMPInfo.biHeight),       1, bitmap_file);
+    fread(&bitmap->BMPInfo.biPlanes,        sizeof(bitmap->BMPInfo.biPlanes),       1, bitmap_file);
+    fread(&bitmap->BMPInfo.biBitCount,      sizeof(bitmap->BMPInfo.biBitCount),     1, bitmap_file);
+    
+    // Reading V-3
+    if (bitmap->BMPInfo.biSize >= 40) {
+        fread(&bitmap->BMPInfo.biCompression,   sizeof(bitmap->BMPInfo.biCompression),      1, bitmap_file);
+        fread(&bitmap->BMPInfo.biSizeImage,     sizeof(bitmap->BMPInfo.biSizeImage),        1, bitmap_file);
+        fread(&bitmap->BMPInfo.biXPelsPerMeter, sizeof(bitmap->BMPInfo.biXPelsPerMeter),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biYPelsPerMeter, sizeof(bitmap->BMPInfo.biYPelsPerMeter),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biClrUsed,       sizeof(bitmap->BMPInfo.biClrUsed),          1, bitmap_file);
+        fread(&bitmap->BMPInfo.biClrImportant,  sizeof(bitmap->BMPInfo.biClrImportant),     1, bitmap_file);
+    }
+
+    // Reading V-3 with color masks
+    if (bitmap->BMPInfo.biSize >= 52) {
+        fread(&bitmap->BMPInfo.biRedMask,       sizeof(bitmap->BMPInfo.biRedMask),      1, bitmap_file);
+        fread(&bitmap->BMPInfo.biGreenMask,     sizeof(bitmap->BMPInfo.biGreenMask),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biBlueMask,      sizeof(bitmap->BMPInfo.biBlueMask),     1, bitmap_file);
+    }
+
+    // Reading V-3 with alpha masks
+    if (bitmap->BMPInfo.biSize >= 56) {
+        fread(&bitmap->BMPInfo.biAlphaMask,     sizeof(bitmap->BMPInfo.biAlphaMask),    1, bitmap_file);
+    }
+
+    // Reading V-4
+    if (bitmap->BMPInfo.biSize >= 108) {
+        fread(&bitmap->BMPInfo.biRedMask,       sizeof(bitmap->BMPInfo.biRedMask),      1, bitmap_file);
+        fread(&bitmap->BMPInfo.biGreenMask,     sizeof(bitmap->BMPInfo.biGreenMask),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biBlueMask,      sizeof(bitmap->BMPInfo.biBlueMask),     1, bitmap_file);
+        fread(&bitmap->BMPInfo.biAlphaMask,     sizeof(bitmap->BMPInfo.biAlphaMask),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biCSType,        sizeof(bitmap->BMPInfo.biCSType),       1, bitmap_file);
+        fread(&bitmap->BMPInfo.biEndpoints,     sizeof(bitmap->BMPInfo.biEndpoints),    1, bitmap_file);
+        fread(&bitmap->BMPInfo.biGammaRed,      sizeof(bitmap->BMPInfo.biGammaRed),     1, bitmap_file);
+        fread(&bitmap->BMPInfo.biGammaGreen,    sizeof(bitmap->BMPInfo.biGammaGreen),   1, bitmap_file);
+        fread(&bitmap->BMPInfo.biGammaBlue,     sizeof(bitmap->BMPInfo.biGammaBlue),    1, bitmap_file);
+    }
+
+    // Reading V-5
+    if (bitmap->BMPInfo.biSize >= 124) {
+        fread(&bitmap->BMPInfo.biIntent,        sizeof(bitmap->BMPInfo.biIntent),         1, bitmap_file);
+        fread(&bitmap->BMPInfo.biProfileData,   sizeof(bitmap->BMPInfo.biProfileData),         1, bitmap_file);
+        fread(&bitmap->BMPInfo.biProfileSize,   sizeof(bitmap->BMPInfo.biProfileSize),         1, bitmap_file);
+        fread(&bitmap->BMPInfo.biReserved,      sizeof(bitmap->BMPInfo.biReserved),         1, bitmap_file);
+    }
+
+    fclose(bitmap_file);
 }
